b312166e2d6905cf5c5f020cd57bc6b1c7f2876a
twwli <hello@theworldwelivein.co.uk>
2017-05-31 11:56:16 +0200

    Small text edits

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ WEBSITE PRELOADER
 ****************************************************/
 
 $(window).on('load', function() { // makes sure the whole site is loaded 
-  $('#status').delay(1500).fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(1800).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('#status').delay(500).fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(800).fadeOut('slow'); // will fade out the white DIV that covers the website. 
   //$('body').delay(500).css({'overflow':'visible'});
 })
 
